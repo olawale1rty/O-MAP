0c7ff3f6331ebd04fb976cec21588cb5391558ef
Mustapha Moshood <olawale3415@gmail.com>
2020-05-29 16:42:55 +0100

    settings

--- gis/settings.py
+++ gis/settings.py
@@ -133,9 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'environment/static/environment'),
+    os.path.join(BASE_DIR, 'shop/static/shop'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'environment/static/environment')
-
 
 # gdal location
 #GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal300'
