cfb18c533ece048333d51f3ff4d0bd3577d8c4db
Mustapha Moshood <olawale3415@gmail.com>
2020-05-30 11:36:16 +0100

    favicon

--- gis/settings.py
+++ gis/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'environment/static/environment'),
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'environment/static/environment'),
     
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'environment/static/environment')
+
 STATIC_URL = '/static/'
 
 # gdal location
